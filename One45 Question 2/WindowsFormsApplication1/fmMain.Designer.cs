01c03507174ff8553e60c7d8a52c8d96838460aa
JeromeVancouver <j_illy@hotmail.com>
2014-08-21 11:33:26 -0700

    Initial Upload

---
+++ One45 Question 2/WindowsFormsApplication1/fmMain.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.btBrowse = new System.Windows.Forms.Button();
+            this.lbOutput = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(13, 13);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(715, 20);
+            this.tbFile.TabIndex = 0;
+            // 
+            // btBrowse
+            // 
+            this.btBrowse.Location = new System.Drawing.Point(734, 10);
+            this.btBrowse.Name = "btBrowse";
+            this.btBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btBrowse.TabIndex = 1;
+            this.btBrowse.Text = "Browse . . . ";
+            this.btBrowse.UseVisualStyleBackColor = true;
+            this.btBrowse.Click += new System.EventHandler(this.btBrowse_Click);
+            // 
+            // lbOutput
+            // 
+            this.lbOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbOutput.FormattingEnabled = true;
+            this.lbOutput.ItemHeight = 20;
+            this.lbOutput.Location = new System.Drawing.Point(13, 39);
+            this.lbOutput.Name = "lbOutput";
+            this.lbOutput.Size = new System.Drawing.Size(796, 464);
+            this.lbOutput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 525);
+            this.Controls.Add(this.lbOutput);
+            this.Controls.Add(this.btBrowse);
+            this.Controls.Add(this.tbFile);
+            this.Name = "Form1";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Button btBrowse;
+        private System.Windows.Forms.ListBox lbOutput;
+    }
+}
+
---
+++ One45 Question 2/WindowsFormsApplication1/fmMain.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class Shape
+        {
+            public string type;
+            public string length;
+            public string perimeter;
+            public string area;
+
+            public Shape()
+            {
+            }
+
+            public Shape(string t, string l)
+            {
+                type = t;
+                length = l;
+            }
+
+            public string GetOutput()
+            {
+                string output;
+                output = "A " + type + " with side length " + this.length + " u has a perimeter of " + this.perimeter +
+                        " u and an area of " + this.area + " u^2";
+                return output;
+            }
+
+        }
+
+        public class Circle : Shape
+        {
+
+            public Circle(string l) 
+                : base("circle", l)
+            {
+                double p = 2 * Math.PI * Convert.ToDouble(this.length);
+                p = Math.Round(p, 2);
+                perimeter = p.ToString();
+                double a = Convert.ToDouble(this.length) * Convert.ToDouble(this.length) * Math.PI;
+                a = Math.Round(a, 2);
+                area = a.ToString();
+            }
+
+            public string GetOutput()
+            {
+                string output;
+                output = "A circle with radius " + this.length + " u has a perimeter of " + this.perimeter +
+                        " u and an area of " + this.area + " u^2";
+                return output;
+            }
+
+        }
+
+        public class Triangle : Shape
+        {
+            public Triangle(string l)
+                : base("triangle", l)
+            {
+                double p = 3 * Convert.ToDouble(this.length);
+                p = Math.Round(p, 2);
+                perimeter = p.ToString();
+                double a = (Math.Sqrt(3) / 4) * Convert.ToDouble(this.length) * Convert.ToDouble(this.length);
+                a = Math.Round(a, 2);
+                area = a.ToString();
+            }
+
+        }
+
+        public class Square : Shape
+        {
+            public Square(string l)
+                : base("square", l)
+            {
+                double p = 4 * Convert.ToDouble(this.length);
+                p = Math.Round(p, 2);
+                perimeter = p.ToString();
+                double a = Convert.ToDouble(this.length) * Convert.ToDouble(this.length);
+                a = Math.Round(a, 2);
+                area = a.ToString();
+            }
+
+        }
+
+        public class Pentagon : Shape
+        {
+            public Pentagon(string l)
+                : base("pentagon", l)
+            {
+                double p = 5 * Convert.ToDouble(this.length);
+                p = Math.Round(p, 2);
+                perimeter = p.ToString();
+                double sqr = 5 * (5 + 2 * Math.Sqrt(5));
+                double a = (0.25) * Math.Sqrt(sqr) * ((Convert.ToDouble(this.length) * Convert.ToDouble(this.length)));
+                a = Math.Round(a, 2);
+                area = a.ToString();
+            }
+
+        }
+
+        private void btBrowse_Click(object sender, EventArgs e)
+        {
+            tbFile.Text = "";
+            lbOutput.Items.Clear();
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "*.csv | *.*";
+            dialog.InitialDirectory = Directory.GetCurrentDirectory().ToString();
+            dialog.Title = "Select CSV File";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                string filename = dialog.FileName;
+                tbFile.Text = filename;
+                var reader = new System.IO.StreamReader(File.OpenRead(filename));
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string output = "Default";
+                    if(line.Equals(""))
+                    {
+                        output = "No Data Found";
+                    }
+                    else
+                    {
+                        var values = line.Split(',');
+                        string shape = values[0];
+                        string length = values[1];
+                        double n;
+                        bool isNumeric = double.TryParse(length, out n);
+                        if (isNumeric)
+                        {
+                            switch (shape)
+                            {
+                                case "circle":
+                                    Circle c = new Circle(length);
+                                    output = c.GetOutput();
+                                    break;
+                                case "triangle":
+                                    Triangle t = new Triangle(length);
+                                    output = t.GetOutput();
+                                    break;
+                                case "square":
+                                    Square s = new Square(length);
+                                    output = s.GetOutput();
+                                    break;
+                                case "pentagon":
+                                    Pentagon p = new Pentagon(length);
+                                    output = p.GetOutput();
+                                    break;
+                                default:
+                                    // You can use the default case.
+                                    output = "Shape " + shape + " is not a valid shape";
+                                    break;
+                            }
+                        }
+                        else
+                            output = "Shape " + shape + " does not have a numeric value ( " +  length + " )";
+
+                    }
+                    lbOutput.Items.Add(output);
+                }
+            } 
+        }
+    }
+}
