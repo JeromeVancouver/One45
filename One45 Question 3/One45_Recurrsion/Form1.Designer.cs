c7d23d716d914fb910be07ac69a0fd8ee2490c3c
JeromeVancouver <j_illy@hotmail.com>
2014-08-21 12:05:34 -0700

    Update Question 2

---
+++ One45 Question 3/One45_Recurrsion/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace One45_Recurrsion
+{
+    partial class fmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbRecur = new System.Windows.Forms.ListBox();
+            this.btGo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbRecur
+            // 
+            this.lbRecur.FormattingEnabled = true;
+            this.lbRecur.Location = new System.Drawing.Point(12, 38);
+            this.lbRecur.Name = "lbRecur";
+            this.lbRecur.Size = new System.Drawing.Size(177, 277);
+            this.lbRecur.TabIndex = 0;
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(13, 13);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 1;
+            this.btGo.Text = "Go";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(197, 323);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.lbRecur);
+            this.Name = "fmMain";
+            this.Text = "Main";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbRecur;
+        private System.Windows.Forms.Button btGo;
+    }
+}
+
---
+++ One45 Question 3/One45_Recurrsion/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace One45_Recurrsion
+{
+    public partial class fmMain : Form
+    {
+        public fmMain()
+        {
+            InitializeComponent();
+        }
+
+        private string GetTabs(int t)
+        {
+            string tString = "";
+            for (int i = 0; i < t; i++)
+                tString += "   ";
+            return tString;
+        }
+
+        private void PrintArray(string[] arr, int level)
+        {
+            int maxSize = arr.Length-1;
+            string tab = GetTabs(level);
+            lbRecur.Items.Add(tab + "<" + arr[level] + ">");
+            if (level < maxSize)
+            {
+                PrintArray(arr, ++level);
+                lbRecur.Items.Add(tab + "</" + arr[level] + ">");
+            } 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string[] theArray = { "a", "b", "c", "d", "e", "f" };
+            PrintArray(theArray, 0);
+        }
+    }
+}
